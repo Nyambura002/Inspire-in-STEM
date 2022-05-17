d512e4f734c4e7152412346faea1deda1fb0789b
Nyambura002 <faithnjuguna2003@gmail.com>
2022-05-17 22:31:56 +0300

    Learnt about data types

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+#calculate the area of a circle
+
+#volume of a cube
+
+#let use type input
+
+radius= input("Enter the radius of a circle")
+pi=3.142
+radius= int(radius)
+print(pi*(radius*radius))
+
+#volume of a cube
+length= input("Enter the length of the cube")
+enterLengthOfCube = 14
+enterLengthOfCube = 14
+enterLengthOfCube = 14
+volume = pi * enterLengthOfCube * enterLengthOfCube * enterLengthOfCube
+print(" The volume of the cube is " +str(volume))
+
---
+++ lesson2.py
@@ -0,0 +1,21 @@
+#my age is an integer-positive/negative whole numbers
+
+
+
+noOfSibling=4
+
+dateOfBirth=28
+
+yearOfBirth=2003
+myAgeIs=18
+favouriteFood="Rice"
+schoolAttended="Maryhill"
+
+#strings
+my_first_name="Faith"
+my_second_name="Nyambura"
+my_last_name="Njuguna"
+
+print("I am " + my_first_name)
+print("I am " + str(myAgeIs))
+
