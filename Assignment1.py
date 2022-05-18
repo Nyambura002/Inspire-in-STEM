9861e71eba67683187a22f9a66e609a0194a2275
Nyambura002 <faithnjuguna2003@gmail.com>
2022-05-18 08:37:01 +0300

    Learning about data types

--- Assignment1.py
+++ Assignment1.py
@@ -1,9 +1,9 @@
 #calculate the area of a circle
 
-#volume of a cube
 
 #let use type input
 
+#are of a circle
 radius= input("Enter the radius of a circle")
 pi=3.142
 radius= int(radius)
@@ -17,3 +17,21 @@ enterLengthOfCube = 14
 volume = pi * enterLengthOfCube * enterLengthOfCube * enterLengthOfCube
 print(" The volume of the cube is " +str(volume))
 
+
+#volume of a cylinder
+radius= input("Enter the radius of a circle")
+pi=3.142
+radius= int(radius)
+enterHeightOfCylinder= 15
+volume = pi * radius * radius * enterHeightOfCylinder
+print(" The volume of the cylinder is " +str(volume))
+
+#surface area of an open cylinder
+radius= input(" Enter the radius of a circle")
+pi=3.142
+radius= int(radius)
+enterHeightOfCylinder = 15
+diametre = input(" Enter the diametre of a circle")
+diametre= int(diametre)
+surfacearea =(pi * diametre * enterHeightOfCylinder)
+print(" The surfacearea of an open cylinder is " +str(surfacearea))
