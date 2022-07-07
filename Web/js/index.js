68b901adc9774f8433ea637d67401920d3e70d22
Nyambura002 <faithnjuguna2003@gmail.com>
2022-07-07 09:26:52 +0300

    Javasciprt

---
+++ Web/js/index.js
@@ -0,0 +1,14 @@
+let message = "Hello World"   //display it within your console//
+console.log(message)
+document.write(message)    //display it within your document//
+
+/* 
+Multiline comment in Javascript
+*/
+
+let num1 = 134
+let num2 = 134
+
+function submitButton(){
+    alert("Button has been clicked")
+}
